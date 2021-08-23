dda706a4e491dc8deb2193c22ffc4d0059d7520a
stefan hall <stefhall@comcast.net>
2021-08-22 19:26:41 -0700

    Initial Commit

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,52 @@
+// import gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create typeDefs
+const typeDefs = gql`
+    type User {
+         _id: ID
+         username: String
+         email: String
+         bookCount: Int
+         savedBooks: [Book]
+    }
+
+    type Book {
+        _id: ID
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input SavedBookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: SavedBookInput): User
+        removeBook(bookId: String!): User
+
+    }
+`;
+
+// export typeDefs
+module.exports = typeDefs;
